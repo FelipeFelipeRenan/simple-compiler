0f2487132bfb656dd09dc433dcdafa9e6e1089cc
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-04-24 14:10:29 -0300

    fix: still fixing functions

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	startingTime := time.Now()
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Uso: go run cmd/main.go <arquivo>")
+		fmt.Fprintln(os.Stderr, "Uso: ./main <arquivo>")
 		os.Exit(1)
 	}
 	fileName := os.Args[1]
